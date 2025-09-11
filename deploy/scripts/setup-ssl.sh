45a41c9d7672dbdaa8cd96d85fa12c93b7c92942
soheeGit <63wlsthgml@gmail.com>
2025-09-12 01:12:37 +0900

    [fix] setup 수정

--- deploy/scripts/setup-ssl.sh
+++ deploy/scripts/setup-ssl.sh
@@ -32,7 +32,7 @@ EOF
 
 # Docker Compose로 서비스 시작 (SSL 없이)
 echo "📦 임시로 서비스를 시작합니다..."
-docker-compose up -d nginx krocs-backend redis
+docker-compose -f docker-compose.prod.yml up -d nginx krocs-backend redis
 
 # SSL 인증서 발급
 echo "📜 SSL 인증서를 발급받습니다..."
