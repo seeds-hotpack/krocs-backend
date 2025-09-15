6a540a91ee0034652af74660bf2d1fd309c1ed62
yeo-li <parkseongyeol2110@gmail.com>
2025-09-15 13:09:34 +0900

    [feat] 예외상황 정리(#119)

--- backend/src/main/java/com/hotpack/krocs/global/common/response/code/resultCode/ErrorStatus.java
+++ backend/src/main/java/com/hotpack/krocs/global/common/response/code/resultCode/ErrorStatus.java
@@ -27,7 +27,9 @@ public enum ErrorStatus implements BaseCode {
     // 비즈니스 로직 에러
     BUSINESS_LOGIC_ERROR(HttpStatus.BAD_REQUEST, "BUSINESS400", "비즈니스 로직 오류"),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "RESOURCE404", "요청한 리소스를 찾을 수 없습니다"),
-    DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "DUPLICATE409", "중복된 리소스입니다");
+    DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "DUPLICATE409", "중복된 리소스입니다"),
+    OAUTH2_SESSION_SERIALIZE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "OAUTH2_500",
+        "세션 직렬화에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
