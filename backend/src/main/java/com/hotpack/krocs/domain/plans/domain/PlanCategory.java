720f492a92373309953347724c51cc998aec8957
gumguard <gawonlee0712@gmail.com>
2025-09-17 00:02:14 +0900

    [chore] PlanCategory 추가

--- backend/src/main/java/com/hotpack/krocs/domain/plans/domain/PlanCategory.java
+++ backend/src/main/java/com/hotpack/krocs/domain/plans/domain/PlanCategory.java
@@ -1,7 +1,15 @@
 package com.hotpack.krocs.domain.plans.domain;
 
 public enum PlanCategory {
-    WORK,  // 업무
-    STUDY,  // 학습
-    ETC   // 카테고리 없음, DEFAULT
+    WORK,      // 업무
+    STUDY,     // 학습
+    WORKOUT,   // 운동
+    REST,      // 휴식
+    HEALTH,    // 건강
+    IMPORTANT, // 중요
+    ENERGY,    // 에너지
+    MUSIC,     // 음악
+    PHOTO,     // 사진
+    GAME,      // 게임
+    ETC        // 카테고리 없음, DEFAULT
 }
