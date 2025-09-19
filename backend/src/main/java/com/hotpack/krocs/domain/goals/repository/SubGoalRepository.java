4961e8136448beb32ecc136d2b25311e8329e576
yeo-li <parkseongyeol2110@gmail.com>
2025-09-19 16:31:48 +0900

    [feat] existsValidSubGoal 추가(#111)

--- backend/src/main/java/com/hotpack/krocs/domain/goals/repository/SubGoalRepository.java
+++ backend/src/main/java/com/hotpack/krocs/domain/goals/repository/SubGoalRepository.java
@@ -5,6 +5,7 @@ import com.hotpack.krocs.domain.goals.domain.SubGoal;
 import com.hotpack.krocs.global.common.entity.Status;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,5 +14,20 @@ public interface SubGoalRepository extends JpaRepository<SubGoal, Long> {
     List<SubGoal> findSubGoalsByGoalAndStatus(Goal goal, Status status);
 
     SubGoal findSubGoalsBySubGoalIdAndStatus(Long subGoalId, Status status);
+
+    @Query(value = """
+        SELECT EXISTS (
+            SELECT s
+            FROM sub_goal s
+            JOIN goals g ON s.goal_id = g.goal_id
+            JOIN users u ON g.user_id = u.user_id
+            WHERE s.sub_goal_id = :subGoalId
+              AND g.goal_id = :goalId
+              AND u.user_id = :userId
+              AND s.status = :status
+        )
+        """, nativeQuery = true)
+    boolean existsValidSubGoal(Long userId,
+        Long goalId, Long subGoalId, Status status);
 
 }
