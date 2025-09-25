62ace31edaf9f7f9ffbd12164875efaaa94909aa
yeo-li <parkseongyeol2110@gmail.com>
2025-09-25 20:48:42 +0900

    [feat] dto 필드 추가(#124)
    isTimeSelected, startDateTime, endDateTime을 추가했습니다.

--- backend/src/main/java/com/hotpack/krocs/domain/goals/dto/response/SubGoalResponseDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/goals/dto/response/SubGoalResponseDTO.java
@@ -1,6 +1,9 @@
 package com.hotpack.krocs.domain.goals.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,9 +12,22 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SubGoalResponseDTO {
 
-  private final Long subGoalId;
+    private final Long subGoalId;
 
-  private final String title;
+    private final String title;
 
-  private final Boolean isCompleted;
+
+    @JsonProperty("is_completed")
+    private final Boolean isCompleted;
+
+    @JsonProperty("is_time_selected")
+    private final Boolean isTimeSelected;
+
+    @JsonProperty("start_date_time")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private final LocalDateTime startDateTime;
+
+    @JsonProperty("end_date_time")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private final LocalDateTime endDateTime;
 }
--- backend/src/main/java/com/hotpack/krocs/domain/goals/dto/response/SubGoalUpdateResponseDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/goals/dto/response/SubGoalUpdateResponseDTO.java
@@ -1,5 +1,6 @@
 package com.hotpack.krocs.domain.goals.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import lombok.Builder;
@@ -9,19 +10,30 @@ import lombok.Getter;
 @Getter
 public class SubGoalUpdateResponseDTO {
 
-  @JsonProperty("sub_goal_id")
-  private Long subGoalId;
+    @JsonProperty("sub_goal_id")
+    private Long subGoalId;
 
-  @JsonProperty("goal_id")
-  private Long goalId;
+    @JsonProperty("goal_id")
+    private Long goalId;
 
-  private String title;
-  @JsonProperty("is_completed")
-  private Boolean isCompleted;
+    private String title;
+    @JsonProperty("is_completed")
+    private Boolean isCompleted;
 
-  @JsonProperty("created_at")
-  private LocalDateTime createdAt;
+    @JsonProperty("is_time_selected")
+    private final Boolean isTimeSelected;
 
-  @JsonProperty("updated_at")
-  private LocalDateTime updatedAt;
+    @JsonProperty("start_date_time")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private final LocalDateTime startDateTime;
+
+    @JsonProperty("end_date_time")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private final LocalDateTime endDateTime;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
 }
