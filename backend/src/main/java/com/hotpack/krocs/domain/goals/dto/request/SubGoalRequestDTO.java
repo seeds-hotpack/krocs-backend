1fb47bfba32119a20b202782b9da595902afc472
yeo-li <parkseongyeol2110@gmail.com>
2025-10-03 15:03:01 +0900

    [refactor] SubGoalRequestDTO 필드 추가(#151)

--- backend/src/main/java/com/hotpack/krocs/domain/goals/dto/request/SubGoalRequestDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/goals/dto/request/SubGoalRequestDTO.java
@@ -1,9 +1,11 @@
 package com.hotpack.krocs.domain.goals.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hotpack.krocs.global.common.constant.ValidationConstants;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,8 +18,17 @@ import lombok.NoArgsConstructor;
 @Getter
 public class SubGoalRequestDTO {
 
-  @NotBlank(message = "{subgoal.title.notBlank}")
-  @Size(max = ValidationConstants.SUB_TITLE_MAX, message = "{subgoal.title.size}")
-  @Schema(description = "SubGoal 제목", example = "Liquibase 이슈 해결")
-  private String title;
+    @NotBlank(message = "{subgoal.title.notBlank}")
+    @Size(max = ValidationConstants.SUB_TITLE_MAX, message = "{subgoal.title.size}")
+    @Schema(description = "SubGoal 제목", example = "Liquibase 이슈 해결")
+    private String title;
+
+    @JsonProperty("is_time_selected")
+    private Boolean isTimeSelected;
+
+    @JsonProperty("start_date_time")
+    private LocalDateTime startDateTime;
+
+    @JsonProperty("end_date_time")
+    private LocalDateTime endDateTime;
 }
