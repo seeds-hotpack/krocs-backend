125a452f8b950954261f451e257e05eeab1b34a2
soheeGit <63wlsthgml@gmail.com>
2025-10-09 21:29:46 +0900

    [feat] 도메인 추가

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/domain/StopwatchStatus.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/domain/StopwatchStatus.java
@@ -1,4 +1,8 @@
 package com.hotpack.krocs.domain.stopwatch.domain;
 
 public enum StopwatchStatus {
+    RUNNING,    // 실행중
+    PAUSED,     // 일시정지
+    COMPLETED,  // 완료
+    CANCELLED   // 취소됨
 }
