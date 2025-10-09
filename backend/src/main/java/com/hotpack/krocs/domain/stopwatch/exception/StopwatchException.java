c6f836c8946ef6337dc887d89fda746ffa916063
soheeGit <63wlsthgml@gmail.com>
2025-10-09 21:26:53 +0900

    [exception] exception 추가

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/exception/StopwatchException.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/exception/StopwatchException.java
@@ -1,7 +1,15 @@
 package com.hotpack.krocs.domain.stopwatch.exception;
 
-public class StopwatchException extends RuntimeException {
-  public StopwatchException(String message) {
+import com.hotpack.krocs.global.common.response.exception.GeneralException;
+import lombok.Getter;
+
+@Getter
+public class StopwatchException extends GeneralException {
+
+  private final StopwatchExceptionType stopwatchExceptionType;
+
+  public StopwatchException(StopwatchExceptionType message) {
     super(message);
+    this.stopwatchExceptionType = message;
   }
 }
--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/exception/StopwatchExceptionType.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/exception/StopwatchExceptionType.java
@@ -1,4 +1,42 @@
 package com.hotpack.krocs.domain.stopwatch.exception;
 
-public enum StopwatchExceptionType {
+import com.hotpack.krocs.global.common.response.code.BaseCode;
+import com.hotpack.krocs.global.common.response.code.Reason;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum StopwatchExceptionType implements BaseCode {
+    STOPWATCH_NOT_FOUND(HttpStatus.NOT_FOUND, "STOPWATCH404", "스톱워치를 찾을 수 없습니다."),
+    STOPWATCH_ALREADY_RUNNING(HttpStatus.CONFLICT, "STOPWATCH409", "이미 실행중인 스톱워치가 있습니다."),
+    STOPWATCH_NOT_RUNNING(HttpStatus.BAD_REQUEST, "STOPWATCH400", "실행중인 스톱워치가 없습니다."),
+    STOPWATCH_ALREADY_COMPLETED(HttpStatus.BAD_REQUEST, "STOPWATCH400", "이미 완료된 스톱워치입니다."),
+    INVALID_STOPWATCH_ACTION(HttpStatus.BAD_REQUEST, "STOPWATCH400", "유효하지 않은 스톱워치 동작입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public Reason getReason() {
+        return Reason.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .data("")
+                .build();
+    }
+
+    @Override
+    public Reason getReasonHttpStatus() {
+        return Reason.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .data("")
+                .build();
+    }
 }
