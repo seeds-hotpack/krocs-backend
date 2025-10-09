4b4bd2f05052aa377a6dce2b1ed6dd28d17e5ed6
soheeGit <63wlsthgml@gmail.com>
2025-10-09 21:30:27 +0900

    [feat] 응답, 요청 형식 작성

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/dto/response/StopwatchTimeResponseDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/dto/response/StopwatchTimeResponseDTO.java
@@ -1,4 +1,12 @@
 package com.hotpack.krocs.domain.stopwatch.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StopwatchTimeResponseDTO {
+    private String time; // "hh:mm:ss" 형식
 }
