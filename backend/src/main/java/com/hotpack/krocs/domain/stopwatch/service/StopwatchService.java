0f63ca4705fca2c3494dd8157a98a44bfba67e00
soheeGit <63wlsthgml@gmail.com>
2025-10-09 21:32:19 +0900

    [feat] 서비스 초기세팅

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/service/StopwatchService.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/service/StopwatchService.java
@@ -1,4 +1,10 @@
 package com.hotpack.krocs.domain.stopwatch.service;
 
+import com.hotpack.krocs.domain.stopwatch.dto.request.StopwatchActionRequestDTO;
+import com.hotpack.krocs.domain.stopwatch.dto.response.StopwatchTimeResponseDTO;
+
 public interface StopwatchService {
+    StopwatchTimeResponseDTO updateStopwatch(Long goalId, Long subgoalId, StopwatchActionRequestDTO request, Long userId);
+
+    StopwatchTimeResponseDTO getStopwatch(Long goalId, Long subgoalId, Long userId);
 }
--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/service/StopwatchServiceImpl.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/service/StopwatchServiceImpl.java
@@ -1,4 +1,24 @@
 package com.hotpack.krocs.domain.stopwatch.service;
 
-public class StopwatchServiceImpl {
+import com.hotpack.krocs.domain.stopwatch.dto.request.StopwatchActionRequestDTO;
+import com.hotpack.krocs.domain.stopwatch.dto.response.StopwatchTimeResponseDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StopwatchServiceImpl implements StopwatchService {
+    @Override
+    public StopwatchTimeResponseDTO updateStopwatch(Long goalId, Long subgoalId, StopwatchActionRequestDTO request, Long userId) {
+        return null;
+    }
+
+    @Override
+    public StopwatchTimeResponseDTO getStopwatch(Long goalId, Long subgoalId, Long userId) {
+        return null;
+    }
 }
