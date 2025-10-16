f74a2174d16e55d75c235d0a3e1cc9c05c14dbe2
soheeGit <63wlsthgml@gmail.com>
2025-10-16 19:24:53 +0900

    [feat] facade 구현

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/facade/StopwatchRepositoryFacade.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/facade/StopwatchRepositoryFacade.java
@@ -1,4 +1,29 @@
 package com.hotpack.krocs.domain.stopwatch.facade;
 
+import com.hotpack.krocs.domain.goals.domain.SubGoal;
+import com.hotpack.krocs.domain.stopwatch.domain.StopwatchLog;
+import com.hotpack.krocs.domain.stopwatch.repository.StopwatchLogRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StopwatchRepositoryFacade {
+
+    private final StopwatchLogRepository stopwatchLogRepository;
+
+    @Transactional
+    public StopwatchLog saveStopwatchLog(StopwatchLog stopwatchLog) {
+        return stopwatchLogRepository.save(stopwatchLog);
+    }
+
+    public List<StopwatchLog> findAllStopwatchLogs(SubGoal subGoal) {
+        return stopwatchLogRepository.findAllBySubGoal(subGoal);
+    }
 }
