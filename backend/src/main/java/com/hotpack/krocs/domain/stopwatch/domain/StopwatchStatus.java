d29d4bccface60ca34143262804ec433ab342c17
soheeGit <63wlsthgml@gmail.com>
2025-10-16 19:09:22 +0900

    [remove] 상태 enum 제거

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/domain/StopwatchStatus.java
+++
@@ -1,8 +0,0 @@
-package com.hotpack.krocs.domain.stopwatch.domain;
-
-public enum StopwatchStatus {
-    RUNNING,    // 실행중
-    PAUSED,     // 일시정지
-    COMPLETED,  // 완료
-    CANCELLED   // 취소됨
-}
