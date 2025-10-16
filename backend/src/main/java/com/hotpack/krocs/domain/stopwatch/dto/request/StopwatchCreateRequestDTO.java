72cb9bcc162364b5d754bd728061e000b190f084
soheeGit <63wlsthgml@gmail.com>
2025-10-16 17:39:06 +0900

    [feat] exception 추가

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/dto/request/StopwatchCreateRequestDTO.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/dto/request/StopwatchCreateRequestDTO.java
@@ -12,11 +12,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-public class StopwatchActionRequestDTO {
+public class StopwatchCreateRequestDTO {
 
     @NotBlank(message = "{stopwatch.action.notBlank}")
     private String action; // "start", "stop", "completed", "clear"
 
+    @NotNull(message = "{stopwatch.date.startRequired}")
     @JsonProperty("start_date_time")
     @Schema(description = "Stopwatch 시작 시간", example = "2025-10-03T10:03")
     private LocalDateTime startDateTime;
@@ -25,10 +26,12 @@ public class StopwatchActionRequestDTO {
     @Schema(description = "Stopwatch 중지 시간", example = "2025-10-03T10:03")
     private LocalDateTime stopDateTime;
 
+    @NotNull(message = "{stopwatch.date.completedRequired}")
     @JsonProperty("completed_date_time")
     @Schema(description = "Stopwatch 완료 시간", example = "2025-10-03T10:30")
     private LocalDateTime completedDateTime;
 
+    @NotNull(message = "{stopwatch.date.clearRequired}")
     @JsonProperty("clear_date_time")
     @Schema(description = "Stopwatch 정리 시간", example = "2025-10-03T10:30")
     private LocalDateTime clearDateTime;
--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/exception/StopwatchExceptionType.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/exception/StopwatchExceptionType.java
@@ -9,11 +9,16 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum StopwatchExceptionType implements BaseCode {
+    STOPWATCH_CREATE_FAILED(HttpStatus.BAD_REQUEST, "STOPWATCH400", "스톱워치 생성이 실패했습니다."),
+    STOPWATCH_FOUND_FAILED(HttpStatus.BAD_REQUEST, "STOPWATCH400", "스톱워치를 찾는 데 실패했습니다."),
+    STOPWATCH_UPDATE_FAILED(HttpStatus.BAD_REQUEST, "STOPWATCH400", "스톱워치 수정이 실패했습니다."),
     STOPWATCH_NOT_FOUND(HttpStatus.NOT_FOUND, "STOPWATCH404", "스톱워치를 찾을 수 없습니다."),
     STOPWATCH_ALREADY_RUNNING(HttpStatus.CONFLICT, "STOPWATCH409", "이미 실행중인 스톱워치가 있습니다."),
     STOPWATCH_NOT_RUNNING(HttpStatus.BAD_REQUEST, "STOPWATCH400", "실행중인 스톱워치가 없습니다."),
     STOPWATCH_ALREADY_COMPLETED(HttpStatus.BAD_REQUEST, "STOPWATCH400", "이미 완료된 스톱워치입니다."),
-    INVALID_STOPWATCH_ACTION(HttpStatus.BAD_REQUEST, "STOPWATCH400", "유효하지 않은 스톱워치 동작입니다.");
+    INVALID_STOPWATCH_ACTION(HttpStatus.BAD_REQUEST, "STOPWATCH400", "유효하지 않은 스톱워치 동작입니다."),
+    STOPWATCH_ALREADY_PAUSED(HttpStatus.BAD_REQUEST, "STOPWATCH400", "이미 일시정지된 스톱워치입니다."),
+    UNAUTHORIZED_STOPWATCH_ACCESS(HttpStatus.FORBIDDEN, "STOPWATCH403", "스톱워치 접근 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
