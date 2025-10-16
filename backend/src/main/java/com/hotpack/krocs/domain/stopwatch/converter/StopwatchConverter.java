efa63d0057d7f6c9f5eb87f565d1aa74e8c1d74e
soheeGit <63wlsthgml@gmail.com>
2025-10-16 19:11:50 +0900

    [feat] converter 추가

--- backend/src/main/java/com/hotpack/krocs/domain/stopwatch/converter/StopwatchConverter.java
+++ backend/src/main/java/com/hotpack/krocs/domain/stopwatch/converter/StopwatchConverter.java
@@ -1,4 +1,28 @@
 package com.hotpack.krocs.domain.stopwatch.converter;
 
+import com.hotpack.krocs.domain.goals.domain.SubGoal;
+import com.hotpack.krocs.domain.stopwatch.domain.StopwatchLog;
+import com.hotpack.krocs.domain.stopwatch.dto.request.StopwatchCreateRequestDTO;
+import com.hotpack.krocs.domain.stopwatch.dto.response.StopwatchTimeResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StopwatchConverter {
+
+    public StopwatchLog toStopwatchLog(SubGoal subGoal, StopwatchCreateRequestDTO request) {
+        return StopwatchLog.builder()
+                .subGoal(subGoal)
+                .startDateTime(request.getStartDateTime())
+                .endDateTime(request.getCompletedDateTime())
+                .elapsedTime(request.getElapsedTime())
+                .build();
+    }
+
+    public StopwatchTimeResponseDTO toStopwatchTimeResponseDTO(StopwatchLog stopwatchLog) {
+        return StopwatchTimeResponseDTO.builder()
+                .startDateTime(stopwatchLog.getStartDateTime())
+                .elapsedTime(stopwatchLog.getElapsedTime())
+                .completedDateTime(stopwatchLog.getEndDateTime())
+                .build();
+    }
 }
